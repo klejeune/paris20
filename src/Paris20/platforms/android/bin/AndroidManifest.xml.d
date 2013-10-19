a83d9aa7c70236223ab93f114868c139766a3412
klejeune <kevin-git@le-jeune.fr>
2013-10-19 19:41:51 +0200

    Environnement Cordova

---
+++ src/Paris20/platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\code\Parmenide\PP\XX\App\src\Paris20\platforms\android\bin\AndroidManifest.xml : \
+C:\code\Parmenide\PP\XX\App\src\Paris20\platforms\android\AndroidManifest.xml \
---
+++ src/Paris20/platforms/android/gen/fr/partipirate/paris20/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package fr.partipirate.paris20;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
