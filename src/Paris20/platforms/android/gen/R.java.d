8fbbac5e573b49c260d745b5d3c5381974ae0a21
klejeune <kevin-git@le-jeune.fr>
2014-02-24 00:29:37 +0100

    Android v0.0.4

--- src/Paris20/platforms/android/gen/R.java.d
+++ src/Paris20/platforms/android/gen/R.java.d
@@ -5,4 +5,8 @@ C:\Users\Alizee\Documents\GitHub\paris20\src\Paris20\platforms\android\res\drawa
 C:\Users\Alizee\Documents\GitHub\paris20\src\Paris20\platforms\android\res\drawable-xhdpi\icon.png \
 C:\Users\Alizee\Documents\GitHub\paris20\src\Paris20\platforms\android\res\values\strings.xml \
 C:\Users\Alizee\Documents\GitHub\paris20\src\Paris20\platforms\android\res\xml\config.xml \
+C:\Users\Alizee\Documents\GitHub\paris20\src\Paris20\platforms\android\bin\res\drawable-hdpi\icon.png \
+C:\Users\Alizee\Documents\GitHub\paris20\src\Paris20\platforms\android\bin\res\drawable-ldpi\icon.png \
+C:\Users\Alizee\Documents\GitHub\paris20\src\Paris20\platforms\android\bin\res\drawable-mdpi\icon.png \
+C:\Users\Alizee\Documents\GitHub\paris20\src\Paris20\platforms\android\bin\res\drawable-xhdpi\icon.png \
 C:\Users\Alizee\Documents\GitHub\paris20\src\Paris20\platforms\android\bin\AndroidManifest.xml \
